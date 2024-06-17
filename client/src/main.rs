1b2a7ebe5f2b24bfa4976ea5b5262df973b875cb
random6 <random6.xyz@gmail.com>
2024-06-17 13:44:52 +0900

    feat: Add some functions in front

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,44 @@
-use iced::{executor, window};
-use iced::widget::{button, checkbox, text, Column};
-use iced::{alignment, Application, Element, Theme, Alignment, Command, Length, Settings};
+use iced::widget::{button, checkbox, text, text_input, Column};
+use iced::{
+    alignment, executor, window, Alignment, Application, Command, Element, Length, Renderer,
+    Settings, Theme,
+};
+use reqwest::{Client, StatusCode};
+
+const URL: &str = "http://127.0.0.1:8080";
+
+// TODO: @random6 implement function
+fn read_image_file(location: String) -> String {
+    String::new()
+}
+
+// TODO: @random6 complete function
+async fn post_data(sender: &Sender) -> StatusCode {
+    let client = Client::new();
+    let params = [
+        ("title", sender.title.clone()),
+        ("text", sender.text.clone()),
+        ("image", sender.image.clone()),
+    ];
+    let response = client.post(URL).json(&params).send().await.unwrap();
+
+    StatusCode::OK
+}
 
 struct Sender {
     now_state: bool,
-    upload_state: bool,
+    title: String,
+    text: String,
+    image: String,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    Toggled(bool),
+    NowToggled(bool),
+    TextTitle(String),
+    TextText(String),
+    TextImage(String),
+    Upload,
     Exit,
 }
 
@@ -23,7 +52,9 @@ impl Application for Sender {
         (
             Sender {
                 now_state: false,
-                upload_state: false,
+                title: String::new(),
+                text: String::new(),
+                image: String::new(),
             },
             Command::none(),
         )
@@ -33,10 +64,27 @@ impl Application for Sender {
         String::from("Now sender")
     }
 
+    // TODO: @random6 repair condition
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            Message::Toggled(value) => {
+            Message::NowToggled(value) => {
                 self.now_state = value;
+                Command::none()
+            }
+            Message::TextTitle(title_input) => {
+                self.title = title_input;
+                Command::none()
+            }
+            Message::TextText(text_text_input) => {
+                self.text = text_text_input;
+                Command::none()
+            }
+            Message::TextImage(image_input) => {
+                self.image = image_input;
+                Command::none()
+            }
+            Message::Upload => {
+                post_data(&self);
                 Command::none()
             }
             Message::Exit => Command::from(window::close(window::Id::MAIN)),
@@ -44,8 +92,35 @@ impl Application for Sender {
     }
 
     fn view(&self) -> Element<Self::Message> {
-        let toggle = checkbox("checkbox", self.now_state)
-            .on_toggle(Message::Toggled);
+        let now_toggle = checkbox("Now Status", self.now_state).on_toggle(Message::NowToggled);
+
+        let title_input: iced::widget::TextInput<'_, Message, Theme, Renderer> =
+            text_input("Title", &self.title)
+                .width(300)
+                .on_input(|s| Message::TextTitle(s))
+                .padding(10);
+
+        let text_text_input: iced::widget::TextInput<'_, Message, Theme, Renderer> =
+            text_input("Text", &self.text)
+                .width(300)
+                .on_input(|s| Message::TextText(s))
+                .padding(10);
+
+        let image_input: iced::widget::TextInput<'_, Message, Theme, Renderer> =
+            text_input("Image", &self.image)
+                .width(300)
+                .on_input(|s| Message::TextImage(s))
+                .padding(10);
+
+        let upload_button = button(
+            text("Upload")
+                .width(Length::Fill)
+                .horizontal_alignment(alignment::Horizontal::Center),
+        )
+        .width(100)
+        .padding(10)
+        .on_press(Message::Upload);
+
         let exit = button(
             text("Exit")
                 .width(Length::Fill)
@@ -58,7 +133,11 @@ impl Application for Sender {
         let content = Column::new()
             .align_items(Alignment::Center)
             .spacing(20)
-            .push(toggle)
+            .push(now_toggle)
+            .push(title_input)
+            .push(text_text_input)
+            .push(image_input)
+            .push(upload_button)
             .push(exit);
 
         Element::from(content)
@@ -66,7 +145,7 @@ impl Application for Sender {
 }
 
 pub fn main() -> iced::Result {
-    let settings: Settings<()> = iced::settings::Settings  {
+    let settings: Settings<()> = iced::settings::Settings {
         window: window::Settings {
             size: iced::Size::new(400.0, 300.0),
             resizable: true,
